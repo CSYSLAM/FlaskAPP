f97b88e7277dd918132fa8b1203fff6575708de4
csy <766671881@qq.com>
2026-06-12 21:14:19 +0800

    add duiwu

--- app.py
+++ app.py
@@ -205,4 +205,7 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=5001, debug=False)
+
+# PythonAnywhere WSGI 需要这个
+application = create_app()
