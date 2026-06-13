934ad2c9a1e2233a8a1919b5b5f0920aed4515a3
csy <766671881@qq.com>
2026-06-13 21:48:04 +0800

    add shangcheng

--- blueprints/shop.py
+++ blueprints/shop.py
@@ -1,10 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for, request, session, flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
-from models.player import EquipmentInstance
 from services import db
-from services.data_service import DataService
 from services.shop_service import ShopService
-from services.equipment_service import EquipmentService
+from services.data_service import DataService
 
 shop_bp = Blueprint('shop', __name__)
 
@@ -12,19 +10,22 @@ shop_bp = Blueprint('shop', __name__)
 @shop_bp.route("/")
 @shop_bp.route("/<shop_id>")
 @login_required
-def shop(shop_id='default'):
+def shop(shop_id='jinzu'):
     player = current_user
-    page = int(request.args.get('page', 1))
-    per_page = int(request.args.get('per_page', 10))
-    shop_data = ShopService.get_shop_data(shop_id, page=page, per_page=per_page)
+    tab = request.args.get('tab', None)
+    shop_data = ShopService.get_shop_data(shop_id, tab=tab)
     if not shop_data:
         flash("商店不存在")
         return redirect(url_for("game.scene"))
+
+    # Build list of all shops for navigation
+    all_shops = ShopService.get_all_shops()
+
     return render_template("shop.html",
                          player=player,
                          shop_data=shop_data,
-                         shop_type=shop_id,
-                         shop_id=shop_id)
+                         shop_id=shop_id,
+                         all_shops=all_shops)
 
 
 @shop_bp.route("/buy/<shop_id>/<item_id>", methods=["POST"])
@@ -37,29 +38,12 @@ def buy_item(shop_id, item_id):
         quantity = 1
     if quantity <= 0:
         quantity = 1
-    page = int(request.args.get('page', 1))
-    per_page = int(request.args.get('per_page', 10))
+
     success, msg = ShopService.buy_item(player, shop_id, item_id, quantity)
     flash(msg)
-    return redirect(url_for('shop.shop', shop_id=shop_id, page=page, per_page=per_page))
 
-
-@shop_bp.route("/buy_equipment/<shop_id>/<template_id>", methods=["POST"])
-@login_required
-def buy_equipment(shop_id, template_id):
-    player = current_user
-    page = int(request.args.get('page', 1))
-    per_page = int(request.args.get('per_page', 10))
-    success, msg = ShopService.buy_equipment(player, shop_id, template_id)
-    flash(msg)
-    return redirect(url_for('shop.shop', shop_id=shop_id, page=page, per_page=per_page))
-
-
-@shop_bp.route("/sell/<item_id>", methods=["POST"])
-@login_required
-def sell_item(item_id):
-    player = current_user
-    quantity = int(request.form.get('quantity', 1))
-    success, msg, price = ShopService.sell_item(player, item_id, quantity)
-    flash(msg)
-    return redirect(request.referrer or url_for('player.inventory'))
+    # Redirect back to same shop/tab
+    tab = request.args.get('tab', '')
+    if tab:
+        return redirect(url_for('shop.shop', shop_id=shop_id, tab=tab))
+    return redirect(url_for('shop.shop', shop_id=shop_id))
