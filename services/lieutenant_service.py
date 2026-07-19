f0fccae79974126e4964d1849041297e1203cf6f
csyhw <132668072+CSYSLAM@users.noreply.github.com>
2026-07-19 11:08:12 +0800

    fix: 一级名将base属性×0.75，降低满属性上限

--- services/lieutenant_service.py
+++ services/lieutenant_service.py
@@ -26,14 +26,14 @@ NORMAL_LIEUTENANT_NAMES = {
 LIEUTENANT_DATA = {
     1: {
         'taishici': {'name': '太史慈', 'gender': 'male', 'class_type': 'mage',
-                     'base_max_health': 140, 'base_max_mana': 150, 'base_attack': 30, 'base_defense': 76,
-                     'base_crit_rate': 0.00160, 'base_dodge_rate': 0.00120},
+                     'base_max_health': 105, 'base_max_mana': 112, 'base_attack': 22, 'base_defense': 57,
+                     'base_crit_rate': 0.00120, 'base_dodge_rate': 0.00090},
         'xuchu': {'name': '许褚', 'gender': 'male', 'class_type': 'warrior',
-                  'base_max_health': 169, 'base_max_mana': 50, 'base_attack': 26, 'base_defense': 95,
-                  'base_crit_rate': 0.00155, 'base_dodge_rate': 0.00115},
+                  'base_max_health': 126, 'base_max_mana': 37, 'base_attack': 19, 'base_defense': 71,
+                  'base_crit_rate': 0.00116, 'base_dodge_rate': 0.00086},
         'zhouyu': {'name': '周瑜', 'gender': 'male', 'class_type': 'assassin',
-                   'base_max_health': 156, 'base_max_mana': 80, 'base_attack': 28, 'base_defense': 83,
-                   'base_crit_rate': 0.00200, 'base_dodge_rate': 0.00150},
+                   'base_max_health': 117, 'base_max_mana': 60, 'base_attack': 21, 'base_defense': 62,
+                   'base_crit_rate': 0.00150, 'base_dodge_rate': 0.00112},
     },
     2: {
         'guanping': {'name': '关平', 'gender': 'male', 'class_type': 'mage',
